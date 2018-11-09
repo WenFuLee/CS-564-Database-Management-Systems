1a2fba337e65a7dfd82b24817849d837e6dbf26d
Wen-Fu Lee <wlee256@wisc.edu>
2018-11-09 14:26:11 -0600

    Add comments

--- PP2/BufMgr/src/buffer.cpp
+++ PP2/BufMgr/src/buffer.cpp
@@ -1,4 +1,10 @@
 /**
+ * Wen-Fu Lee         wlee256@wisc.edu    9077987502
+ * Shang-Yen Yeh      syeh6@wisc.edu      9079956299
+ * Yahn-Chung Chen    chen666@wisc.edu    9075858895
+ *
+ * This file includes all the implementations of Badger buffer manager.
+ *
  * @author See Contributors.txt for code contributors and overview of BadgerDB.
  *
  * @section LICENSE
@@ -39,45 +45,18 @@ BufMgr::BufMgr(std::uint32_t bufs)
 BufMgr::~BufMgr() {
 	for (FrameId i = 0; i < numBufs; i++) {
 		try
-		{	
-			/*
-			if (this->bufDescTable[i].pinCnt > 0) {
-				throw PagePinnedException(this->bufDescTable[i].file->filename(), this->bufDescTable[i].pageNo, i);
-			}
-
-			
-			// not sure if it is necessary to check validation.
-			if (!this->bufDescTable[i].valid) {
-				throw BadBufferException(i,
-										this->bufDescTable[i].dirty,
-										this->bufDescTable[i].valid,
-										this->bufDescTable[i].refbit);
-			}
-			*/
-
+		{
+            // Flushes out all dirty pages
 			if (this->bufDescTable[i].dirty) {
 				this->bufDescTable[i].file->writePage(this->bufPool[i]);
 				this->bufDescTable[i].dirty = false;
 			}
-			/*
-			this->hashTable->remove(this->bufDescTable[i].file, this->bufDescTable[i].pageNo);
-			this->bufDescTable[i].Clear();
-			this->bufPool[i] = Page();
-			*/
 		}
-		/*
-		catch (PagePinnedException) {
-			std::cout << "Page of the file is pinned!" << std::endl;
-		}
-		catch (BadBufferException) {
-			std::cout << "An invalid page belonging to the file is encountered!" << std::endl;
-		}
-		*/
 		catch (...)
 		{
 		}
 	}
-
+    // Deallocates the buffer pool, the BufDesc table, and hashTable
 	delete [] bufDescTable;
     delete [] bufPool;
 	delete hashTable;
@@ -85,17 +64,18 @@ BufMgr::~BufMgr() {
 
 void BufMgr::advanceClock()
 {
+    // Advance clock to next frame in the buffer pool
 	this->clockHand++;
 	this->clockHand %= this->numBufs; 
 }
 
 void BufMgr::allocBuf(FrameId & frame) 
 {
-	int count = 0;
+	int count = 0; /* Used to detect all buffer frames are pinned */
+    // Allocates a free frame using the clock algorithm
 	while (true) {
 		this->advanceClock();
 		frame = this->clockHand;
-
 		if (!this->bufDescTable[frame].valid) {
 			return;
 		} else {
@@ -103,19 +83,19 @@ void BufMgr::allocBuf(FrameId & frame)
 				this->bufDescTable[frame].refbit = false;
 				continue;
 			}
-
 			if (this->bufDescTable[frame].pinCnt > 0) {
 				++count;
 				if (count == numBufs * 2) {
+                    // Throws BufferExceededException if all buffer frames are pinned
 					throw BufferExceededException();
 				}
 				continue;
 			}
-
+            // Writing a dirty page back to disk
 			if (this->bufDescTable[frame].dirty) {
 				this->bufDescTable[frame].file->writePage(this->bufPool[frame]);
 			}
-
+            // If the buffer frame allocated has a valid page in it, remove the entry from the hash table
 			this->hashTable->remove(this->bufDescTable[frame].file, this->bufDescTable[frame].pageNo);
 			this->bufDescTable[frame].Clear();
 			return;
@@ -127,16 +107,24 @@ void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
 	FrameId frameNo;
 	try {
+        // Check whether the page is already in the buffer pool
 		this->hashTable->lookup(file, pageNo, frameNo);
+        
+        // Page is in the buffer pool
 		page = &this->bufPool[frameNo];
-		this->bufDescTable[frameNo].refbit = true; // check whether it should be true or false later.
+		this->bufDescTable[frameNo].refbit = true;
+        // Increment the pinCnt for the page
 		this->bufDescTable[frameNo].pinCnt++;
-	} 
+	}
 	catch (HashNotFoundException)
 	{
+        // Page is not in the buffer pool
 		std::cout << "HashNotFoundException" << std::endl;
-		
+        
+        //Read the page from disk into the buffer pool frame
 		Page diskPage = file->readPage(pageNo);
+        
+        // Allocate a buffer frame
 		this->allocBuf(frameNo);
 		this->hashTable->insert(file, pageNo, frameNo);
 		this->bufDescTable[frameNo].Set(file, pageNo);
@@ -163,11 +151,6 @@ void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty)
 			this->bufDescTable[frameNo].dirty = true;
 		}
 	}
-	/*
-	catch(PageNotPinnedException)
-	{
-		std::cout << "PageNotPinnedException" << std::endl;
-	}*/
 	catch(HashNotFoundException)
 	{
 		std::cout << "HashNotFoundException" << std::endl;
@@ -178,7 +161,7 @@ void BufMgr::flushFile(const File* file)
 {
 	FrameId frameNo;
 	File temp = *file;
-	 // Iterate through all pages in the file.
+    // Iterate through all pages in the file.
     for (FileIterator iter = temp.begin(); iter != temp.end(); ++iter) {
 		try
 		{
@@ -187,23 +170,19 @@ void BufMgr::flushFile(const File* file)
 			if (this->bufDescTable[frameNo].pinCnt > 0) {
 				throw PagePinnedException(file->filename(), pageNo, frameNo);
 			}
-
 			if (!this->bufDescTable[frameNo].valid) {
 				throw BadBufferException(frameNo,
 										this->bufDescTable[frameNo].dirty,
 										this->bufDescTable[frameNo].valid,
 										this->bufDescTable[frameNo].refbit);
 			}
-
 			if (this->bufDescTable[frameNo].dirty) {
 				this->bufDescTable[frameNo].file->writePage(this->bufPool[frameNo]);
 				this->bufDescTable[frameNo].dirty = false;
 			}
 			this->hashTable->remove(file, pageNo);
 			this->bufDescTable[frameNo].Clear();
-			//this->bufPool[frameNo] = Page();
 		}
-		// Not sure if it is ok to interrupt the whole process once exception happened.
 		catch (HashNotFoundException)
 		{
 			std::cout << "Page not in buffer!" << std::endl;
@@ -216,10 +195,12 @@ void BufMgr::flushFile(const File* file)
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
+    // Allocate an empty page in the specified file
 	Page diskPage = file->allocatePage();
-	
 	pageNo = diskPage.page_number();
-	FrameId frameNo;
+    
+	// Obtain a frame from buffer pool and insert the entry(page)
+    FrameId frameNo;
 	this->allocBuf(frameNo);
 	this->hashTable->insert(file, pageNo, frameNo);
 	this->bufDescTable[frameNo].Set(file, pageNo);
@@ -231,15 +212,16 @@ void BufMgr::disposePage(File* file, const PageId PageNo)
 {
 	try {
 		FrameId frameNo;
-		this->hashTable->lookup(file, PageNo, frameNo);
-		if (this->bufDescTable[frameNo].dirty) {
-			//file->writePage(this->bufPool[frameNo]);
+		// Check if the page to be deleted is allocated a frame in the buffer pool
+        this->hashTable->lookup(file, PageNo, frameNo);
+		
+        // Wirte back if the page is dirty, free corresponding table
+        if (this->bufDescTable[frameNo].dirty) {
 			this->bufDescTable[frameNo].file->writePage(this->bufPool[frameNo]);
 		}
 		this->bufDescTable[frameNo].Clear();
 		this->hashTable->remove(file, PageNo);
-		//this->bufPool[frameNo] = Page();
-	} 
+	}
 	catch (HashNotFoundException)
 	{
 		std::cout << "Page not in buffer!" << std::endl;
@@ -248,25 +230,22 @@ void BufMgr::disposePage(File* file, const PageId PageNo)
 	{
 		std::cout << "Unknown exception!" << std::endl;
 	}
-
 	file->deletePage(PageNo);
 }
 
 void BufMgr::printSelf(void) 
 {
-  BufDesc* tmpbuf;
+    BufDesc* tmpbuf;
 	int validFrames = 0;
-  
-  for (std::uint32_t i = 0; i < numBufs; i++)
-	{
-  	tmpbuf = &(bufDescTable[i]);
-		std::cout << "FrameNo:" << i << " ";
-		tmpbuf->Print();
+    for (std::uint32_t i = 0; i < numBufs; i++)
+    {
+        tmpbuf = &(bufDescTable[i]);
+        std::cout << "FrameNo:" << i << " ";
+        tmpbuf->Print();
 
-  	if (tmpbuf->valid == true)
-    	validFrames++;
-  }
-
+        if (tmpbuf->valid == true)
+        validFrames++;
+    }
 	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
 }
 
